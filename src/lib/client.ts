4faf0dba1deb09df1090d42aae4a16c606010b55
Clinton D <3950377+clinton2111@users.noreply.github.com>
2026-01-31 22:55:17 +0400

    Handle Vercel URL

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,5 +1,11 @@
 import { treaty } from "@elysiajs/eden";
 import type { app } from "../app/api/[[...slugs]]/route";
 
+const baseUrl =
+  typeof window === "undefined"
+    ? process.env.NEXT_PUBLIC_BASE_URL ||
+      (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000")
+    : window.location.origin;
+
 // .api to enter /api prefix
-export const client = treaty<typeof app>("localhost:3000").api;
+export const client = treaty<typeof app>(baseUrl).api;
